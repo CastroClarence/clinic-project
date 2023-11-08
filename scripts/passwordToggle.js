eb356d103c10411f07d5f9a81cdd6e5520d6d5d1
Benedict John Comia <bjcomia00@gmail.com>
2023-11-08 22:16:35 +0800

    modify passToggle

--- scripts/passwordToggle.js
+++ scripts/passwordToggle.js
@@ -1,15 +1,11 @@
-function toggleImg(){
+document.getElementById("passImageLog").addEventListener("click", function(){
     let imgPass = document.getElementById("passImageLog");
     let passwordLog = document.getElementById("passwordLogin");
     if(passwordLog.type == "password"){
         passwordLog.type = "text";
         imgPass.src = "../images/showPass.png";
-        console.log("yes");
     }else{
         passwordLog.type = "password";
         imgPass.src = "../images/hidePass.png";
-        console.log("no");
     }
-}
-
-document.getElementById("passImage").addEventListener("click", toggleImg);
+});
