2648418fc57914a45bbf17c7018070c401c8bf10
CastroClarence <21-00390@g.batstate-u.edu.ph>
2023-11-13 15:42:48 +0800

    search functionality

--- pages/appointmentRequest.php
+++ pages/appointmentRequest.php
@@ -1,3 +1,39 @@
+<?php
+  include("../phpFiles/dbConnect.php");
+  session_start();
+  
+ // Initialize variables to store search criteria
+  $searchKeyword = "";
+
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      // Handle form submission
+      $searchKeyword = $_POST["searchKeyword"];
+  }
+
+  if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+  }
+
+  //normal function
+  $sql = "SELECT requests.requestID, requests.patientID, patients.patientFirstName, patients.patientLastName, requests.serviceID, requests.requestDate, requests.requestTime, requests.requestStatus
+          FROM requests
+          LEFT JOIN patients ON requests.patientID = patients.patientID";
+
+  //search if search not empty
+  if (!empty($searchKeyword)) {
+      $sql .= " WHERE patients.patientFirstName LIKE '%$searchKeyword%' OR patients.patientLastName LIKE '%$searchKeyword%'";
+  }
+
+  $sql .= ";";
+
+  $result = $conn->query($sql);
+
+  if (!$result) {
+      die("Error in SQL query: " . $conn->error);
+  }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,46 +52,62 @@
 
       <ul>
 
-        <li><a href="#" class="logo">
+        <li>
+          <a href="#" class="logo">
           <img src="../images/logoDashboard.png">
-          <span class="nav-item">Dra. Ruth Luneta-Alolod
+            <span class="nav-item">Dra. Ruth Luneta-Alolod
             <br>Dental Clinic</span>
-        </a></li>
+          </a>
+        </li>
 
-        <li><a href="staffDashboard.html">
+        <li>
+          <a href="staffDashboard.html">
             <i class="fas fa-house-user"></i>
-          <span class="nav-item">Home</span>
-        </a></li>
+            <span class="nav-item">Home</span>
+          </a>
+        </li>
 
-        <li><a href="addPatient.html">
+        <li>
+          <a href="addPatient.html">
             <i class="fas fa-user-plus"></i>
-          <span class="nav-item">Add Patient</span>
-        </a></li>
+            <span class="nav-item">Add Patient</span>
+          </a>
+        </li>
 
-        <li><a href="appointmentRequest.html">
+        <li>
+          <a href="appointmentRequest.html">
             <i class="fas fa-calendar-check"></i>
-          <span class="nav-item">Appointment 
-            Request </span>
-        </a></li>
-        <li><a href="#">
+            <span class="nav-item">Appointment Request </span>
+          </a>
+        </li>
+
+        <li>
+          <a href="#">
             <i class="fas fa-laptop-medical"></i>
-          <span class="nav-item">Patient Record</span>
-        </a></li>
+            <span class="nav-item">Patient Record</span>
+          </a>
+        </li>
 
-        <li><a href="#">
+        <li>
+          <a href="#">
             <i class="fas fa-clipboard"></i>
-          <span class="nav-item">Treatment Record</span>
-        </a></li>
+            <span class="nav-item">Treatment Record</span>
+          </a>
+        </li>
 
-        <li><a href="#">
+        <li>
+          <a href="#">
             <i class="fas fa-calendar"></i>
             <span class="nav-item">Calendar</span>
-          </a></li>
+          </a>
+        </li>
     
-        <li><a href="#" class="logout">
-          <i class="fas fa-sign-out-alt"></i>
-          <span class="nav-item">Log out</span>
-        </a></li>
+        <li>
+          <a href="#" class="logout">
+            <i class="fas fa-sign-out-alt"></i>
+            <span class="nav-item">Log out</span>
+          </a>
+        </li>
 
       </ul>
 
@@ -68,32 +120,35 @@
         <div id="reqTbl">
 
           <div id="search-container">
-            <form action="">
-              <input type="text" name="search">
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+              <input type="text" name="searchKeyword" value="<?php echo $searchKeyword; ?>">
             </form>
           </div>
 
           <?php
-            include("../phpFiles/dbConnect.php");
-            session_start();
-            
-            if ($conn->connect_error) {
-                die("Connection failed: " . $conn->connect_error);
-            }
-
-            $sql = "SELECT * FROM requests;";
-            $result = $conn->query($sql);
-
-            if (!$result) {
-                die("Error in SQL query: " . $conn->error);
-            }
-
             if ($result->num_rows > 0) {
                 echo "<table>";
-                echo "<tr><th>Request ID</th><th>Patient ID</th><th>Service ID</th><th>Date</th><th>Time</th><th>Status</th><th>Action</th></tr>";
+                echo "<tr><th>Request ID</th><th>Patient ID</th><th>First Name</th><th>Last Name</th><th>Service ID</th><th>Date</th><th>Time</th><th>Status</th><th>Action</th></tr>";
 
                 while ($row = $result->fetch_assoc()) {
-                    echo"<tr><td>{$row['requestID']}</td><td>{$row['patientID']}</td><td>{$row['serviceID']}</td><td>{$row['requestDate']}</td><td>{$row['requestTime']}</td><td>{$row['requestStatus']}</td><td></td></tr>";
+                  echo "<tr>";
+                  echo "<td>" . $row["requestID"] . "</td>";
+                  echo "<td>" . $row["patientID"] . "</td>";
+                  echo "<td>" . $row["patientFirstName"] . "</td>";
+                  echo "<td>" . $row["patientLastName"] . "</td>";
+                  echo "<td>" . $row["serviceID"] . "</td>";
+                  echo "<td>" . $row["requestDate"] . "</td>";
+                  echo "<td>" . $row["requestTime"] . "</td>";
+                  echo "<td>" . $row["requestStatus"] . "</td>";
+
+                  //update and delete buttons
+                  echo "<td>";
+                  echo "<a href='update.php?requestID=" . $row["requestID"] . "'>Update</a>";
+                  echo " ";
+                  echo "<a href='delete.php?requestID=" . $row["requestID"] . "'>Delete</a>";
+                  echo "</td>";
+                  echo "</tr>";
+                  
                 }
                 
                 echo "</table>";
