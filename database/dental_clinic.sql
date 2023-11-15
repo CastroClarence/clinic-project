a6c07b096daaae7b0ebe69c90168bd367b25b680
Benedict John Comia <bjcomia00@gmail.com>
2023-11-15 16:11:27 +0800

    change DB

--- database/dental_clinic.sql
+++ database/dental_clinic.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 14, 2023 at 01:07 PM
+-- Generation Time: Nov 15, 2023 at 09:10 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -76,44 +76,45 @@ CREATE TABLE `patients` (
   `patientEmail` varchar(70) DEFAULT NULL,
   `patientAddress` varchar(100) DEFAULT NULL,
   `patientOccupation` varchar(30) DEFAULT NULL,
-  `patientBalance` decimal(15,2) DEFAULT NULL
+  `patientBalance` decimal(15,2) DEFAULT NULL,
+  `patientStatus` varchar(15) DEFAULT 'Not Verified'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `patients`
 --
 
-INSERT INTO `patients` (`patientID`, `patientFirstName`, `patientLastName`, `patientAge`, `patientSex`, `patientMobileNo`, `patientEmail`, `patientAddress`, `patientOccupation`, `patientBalance`) VALUES
-(200001, 'Benoite', 'Melley', 44, 'F', '0937-839-9538', 'bmelley0@auda.org.au', '984 Di Loreto Center', 'GIS Technical Architect', '0.00'),
-(200002, 'Barrie', 'Meaders', 12, 'M', '0922-109-5888', 'bmeaders1@ameblo.jp', '3 Independence Hill', 'Librarian', '0.00'),
-(200003, 'Wilburt', 'Thurston', 54, 'M', '0924-645-0618', 'wthurston2@ebay.com', '6 Graceland Place', 'GIS Technical Architect', '0.00'),
-(200004, 'Worden', 'Lean', 47, 'M', '0966-241-7510', 'wlean3@printfriendly.com', '278 Jana Avenue', 'Accountant III', '0.00'),
-(200005, 'Zuzana', 'Whittle', 27, 'F', '0922-524-6481', 'zwhittle4@home.pl', '89290 Butterfield Crossing', 'Compensation Analyst', '0.00'),
-(200006, 'Perry', 'Bernadon', 56, 'F', '0954-541-8220', 'pbernadon5@e-recht24.de', '34 Tony Road', 'Help Desk Operator', '0.00'),
-(200007, 'Deny', 'Gover', 50, 'F', '0924-109-4926', 'dgover6@myspace.com', '22 New Castle Park', 'Occupational Therapist', '0.00'),
-(200008, 'Fernande', 'Tewkesbury.', 26, 'F', '0993-757-7386', 'ftewkesbury7@nationalgeographic.com', '4374 Cardinal Crossing', 'Electrical Engineer', '0.00'),
-(200009, 'Mill', 'Clifford', 51, 'M', '0950-252-2893', 'mclifford8@yolasite.com', '8300 Amoth Terrace', 'Financial Advisor', '0.00'),
-(200010, 'Bev', 'Cocker', 20, 'M', '0980-945-1834', 'bcocker9@arizona.edu', '1 Golf View Place', 'Human Resources Assistant IV', '0.00'),
-(200011, 'Jarvis', 'Bewshea', 60, 'M', '0937-626-2573', 'jbewsheaa@sciencedaily.com', '532 Amoth Avenue', 'Senior Cost Accountant', '0.00'),
-(200012, 'Tedie', 'Isbell', 67, 'M', '0956-931-3447', 'tisbellb@fc2.com', '744 Florence Hill', 'Financial Advisor', '0.00'),
-(200013, 'Mamie', 'Van Merwe', 32, 'F', '0903-364-3842', 'mvanmerwec@chron.com', '12 Kensington Terrace', 'Health Coach IV', '0.00'),
-(200014, 'Maurene', 'Wadesworth', 46, 'F', '0932-196-0069', 'mwadesworthd@fc2.com', '6 Barby Center', 'Senior Quality Engineer', '0.00'),
-(200015, 'Marta', 'Gilligan', 68, 'F', '0910-139-9570', 'mgilligane@boston.com', '63275 Autumn Leaf Point', 'Web Developer III', '0.00'),
-(200016, 'Leif', 'Sired', 53, 'M', '0922-305-3955', 'lsiredf@cnet.com', '77756 Farwell Way', 'Environmental Tech', '0.00'),
-(200017, 'Tanya', 'Cuer', 37, 'F', '0935-479-8432', 'tcuerg@goo.gl', '342 Fallview Hill', 'Physical Therapy Assistant', '0.00'),
-(200018, 'Marlene', 'Trimbey', 69, 'F', '0925-813-7172', 'mtrimbeyh@slate.com', '2 Lyons Crossing', 'Occupational Therapist', '0.00'),
-(200019, 'Adele', 'Easey', 11, 'F', '0906-441-6105', 'aeaseyi@stumbleupon.com', '9727 Erie Street', 'Web Developer II', '0.00'),
-(200020, 'Domini', 'Barrasse', 52, 'F', '0909-668-4827', 'dbarrassej@narod.ru', '00586 Swallow Drive', 'Clinical Specialist', '0.00'),
-(200021, 'Melvin', 'De Nisco', 51, 'M', '0951-822-1027', 'mdeniscok@va.gov', '88670 Tony Lane', 'VP Sales', '0.00'),
-(200022, 'Lauretta', 'Godin', 23, 'F', '0956-595-2954', 'lgodinl@furl.net', '9882 Fairfield Parkway', 'Media Manager III', '0.00'),
-(200023, 'Kriste', 'Baudouin', 65, 'F', '0919-684-9821', 'kbaudouinm@zdnet.com', '418 Bluestem Hill', 'Programmer I', '0.00'),
-(200024, 'Zarah', 'Smittoune', 31, 'F', '0991-302-1338', 'zsmittounen@baidu.com', '48938 Eastlawn Crossing', 'Structural Engineer', '0.00'),
-(200025, 'Alaster', 'Granham', 22, 'M', '0973-862-2291', 'agranhamo@columbia.edu', '00345 Stang Drive', 'Analyst Programmer', '0.00'),
-(200026, 'Andreas', 'Wannell', 31, 'M', '0941-481-3357', 'awannellp@vistaprint.com', '70173 Hooker Alley', 'Junior Executive', '0.00'),
-(200027, 'Artie', 'Gildea', 12, 'M', '0944-899-5235', 'agildeaq@marketwatch.com', '8 Fuller Crossing', 'Senior Quality Engineer', '0.00'),
-(200028, 'Leanna', 'Dilleston', 13, 'F', '0943-593-6466', 'ldillestonr@wikia.com', '3073 Memorial Pass', 'Associate Professor', '0.00'),
-(200029, 'Allan', 'Hammerich', 59, 'M', '0968-410-5354', 'ahammerichs@linkedin.com', '3009 Scoville Trail', 'Automation Specialist I', '0.00'),
-(200030, 'Iolanthe', 'Dreossi', 36, 'F', '0922-450-6231', 'idreossit@hexun.com', '3234 8th Point', 'Physical Therapy Assistant', '0.00');
+INSERT INTO `patients` (`patientID`, `patientFirstName`, `patientLastName`, `patientAge`, `patientSex`, `patientMobileNo`, `patientEmail`, `patientAddress`, `patientOccupation`, `patientBalance`, `patientStatus`) VALUES
+(200001, 'Benoite', 'Melley', 44, 'F', '0937-839-9538', 'bmelley0@auda.org.au', '984 Di Loreto Center', 'GIS Technical Architect', '0.00', 'Verified'),
+(200002, 'Barrie', 'Meaders', 12, 'M', '0922-109-5888', 'bmeaders1@ameblo.jp', '3 Independence Hill', 'Librarian', '0.00', 'Verified'),
+(200003, 'Wilburt', 'Thurston', 54, 'M', '0924-645-0618', 'wthurston2@ebay.com', '6 Graceland Place', 'GIS Technical Architect', '0.00', 'Verified'),
+(200004, 'Worden', 'Lean', 47, 'M', '0966-241-7510', 'wlean3@printfriendly.com', '278 Jana Avenue', 'Accountant III', '0.00', 'Verified'),
+(200005, 'Zuzana', 'Whittle', 27, 'F', '0922-524-6481', 'zwhittle4@home.pl', '89290 Butterfield Crossing', 'Compensation Analyst', '0.00', 'Verified'),
+(200006, 'Perry', 'Bernadon', 56, 'F', '0954-541-8220', 'pbernadon5@e-recht24.de', '34 Tony Road', 'Help Desk Operator', '0.00', 'Verified'),
+(200007, 'Deny', 'Gover', 50, 'F', '0924-109-4926', 'dgover6@myspace.com', '22 New Castle Park', 'Occupational Therapist', '0.00', 'Verified'),
+(200008, 'Fernande', 'Tewkesbury.', 26, 'F', '0993-757-7386', 'ftewkesbury7@nationalgeographic.com', '4374 Cardinal Crossing', 'Electrical Engineer', '0.00', 'Verified'),
+(200009, 'Mill', 'Clifford', 51, 'M', '0950-252-2893', 'mclifford8@yolasite.com', '8300 Amoth Terrace', 'Financial Advisor', '0.00', 'Verified'),
+(200010, 'Bev', 'Cocker', 20, 'M', '0980-945-1834', 'bcocker9@arizona.edu', '1 Golf View Place', 'Human Resources Assistant IV', '0.00', 'Verified'),
+(200011, 'Jarvis', 'Bewshea', 60, 'M', '0937-626-2573', 'jbewsheaa@sciencedaily.com', '532 Amoth Avenue', 'Senior Cost Accountant', '0.00', 'Verified'),
+(200012, 'Tedie', 'Isbell', 67, 'M', '0956-931-3447', 'tisbellb@fc2.com', '744 Florence Hill', 'Financial Advisor', '0.00', 'Verified'),
+(200013, 'Mamie', 'Van Merwe', 32, 'F', '0903-364-3842', 'mvanmerwec@chron.com', '12 Kensington Terrace', 'Health Coach IV', '0.00', 'Verified'),
+(200014, 'Maurene', 'Wadesworth', 46, 'F', '0932-196-0069', 'mwadesworthd@fc2.com', '6 Barby Center', 'Senior Quality Engineer', '0.00', 'Verified'),
+(200015, 'Marta', 'Gilligan', 68, 'F', '0910-139-9570', 'mgilligane@boston.com', '63275 Autumn Leaf Point', 'Web Developer III', '0.00', 'Verified'),
+(200016, 'Leif', 'Sired', 53, 'M', '0922-305-3955', 'lsiredf@cnet.com', '77756 Farwell Way', 'Environmental Tech', '0.00', 'Verified'),
+(200017, 'Tanya', 'Cuer', 37, 'F', '0935-479-8432', 'tcuerg@goo.gl', '342 Fallview Hill', 'Physical Therapy Assistant', '0.00', 'Verified'),
+(200018, 'Marlene', 'Trimbey', 69, 'F', '0925-813-7172', 'mtrimbeyh@slate.com', '2 Lyons Crossing', 'Occupational Therapist', '0.00', 'Verified'),
+(200019, 'Adele', 'Easey', 11, 'F', '0906-441-6105', 'aeaseyi@stumbleupon.com', '9727 Erie Street', 'Web Developer II', '0.00', 'Verified'),
+(200020, 'Domini', 'Barrasse', 52, 'F', '0909-668-4827', 'dbarrassej@narod.ru', '00586 Swallow Drive', 'Clinical Specialist', '0.00', 'Verified'),
+(200021, 'Melvin', 'De Nisco', 51, 'M', '0951-822-1027', 'mdeniscok@va.gov', '88670 Tony Lane', 'VP Sales', '0.00', 'Verified'),
+(200022, 'Lauretta', 'Godin', 23, 'F', '0956-595-2954', 'lgodinl@furl.net', '9882 Fairfield Parkway', 'Media Manager III', '0.00', 'Verified'),
+(200023, 'Kriste', 'Baudouin', 65, 'F', '0919-684-9821', 'kbaudouinm@zdnet.com', '418 Bluestem Hill', 'Programmer I', '0.00', 'Verified'),
+(200024, 'Zarah', 'Smittoune', 31, 'F', '0991-302-1338', 'zsmittounen@baidu.com', '48938 Eastlawn Crossing', 'Structural Engineer', '0.00', 'Verified'),
+(200025, 'Alaster', 'Granham', 22, 'M', '0973-862-2291', 'agranhamo@columbia.edu', '00345 Stang Drive', 'Analyst Programmer', '0.00', 'Verified'),
+(200026, 'Andreas', 'Wannell', 31, 'M', '0941-481-3357', 'awannellp@vistaprint.com', '70173 Hooker Alley', 'Junior Executive', '0.00', 'Verified'),
+(200027, 'Artie', 'Gildea', 12, 'M', '0944-899-5235', 'agildeaq@marketwatch.com', '8 Fuller Crossing', 'Senior Quality Engineer', '0.00', 'Verified'),
+(200028, 'Leanna', 'Dilleston', 13, 'F', '0943-593-6466', 'ldillestonr@wikia.com', '3073 Memorial Pass', 'Associate Professor', '0.00', 'Verified'),
+(200029, 'Allan', 'Hammerich', 59, 'M', '0968-410-5354', 'ahammerichs@linkedin.com', '3009 Scoville Trail', 'Automation Specialist I', '0.00', 'Verified'),
+(200030, 'Iolanthe', 'Dreossi', 36, 'F', '0922-450-6231', 'idreossit@hexun.com', '3234 8th Point', 'Physical Therapy Assistant', '0.00', 'Verified');
 
 -- --------------------------------------------------------
 
@@ -130,6 +131,17 @@ CREATE TABLE `requests` (
   `requestNotes` text DEFAULT NULL,
   `requestStatus` varchar(10) DEFAULT 'Pending'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `requests`
+--
+
+INSERT INTO `requests` (`requestID`, `patientID`, `requestServices`, `requestDate`, `requestTime`, `requestNotes`, `requestStatus`) VALUES
+(300001, 200027, 'Tooth Extraction, Kimi', '2023-11-16', '09:30:00', 'Pabunot po', 'Approved'),
+(300002, 200001, 'Kimi lang po', '2023-11-16', '13:00:00', 'Kemeruts', 'Approved'),
+(300003, 200020, 'Kemeruts', '2023-11-17', '14:00:00', 'Test', 'Pending'),
+(300004, 200010, 'Ahahaahaha', '2023-11-17', '10:30:00', 'blehh', 'Approved'),
+(300005, 200023, 'hahahhaha', '2023-11-18', '09:00:00', 'Test', 'Approved');
 
 -- --------------------------------------------------------
 
@@ -206,7 +218,7 @@ ALTER TABLE `patients`
 -- AUTO_INCREMENT for table `requests`
 --
 ALTER TABLE `requests`
-  MODIFY `requestID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=300001;
+  MODIFY `requestID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=300006;
 
 --
 -- AUTO_INCREMENT for table `transactions`
