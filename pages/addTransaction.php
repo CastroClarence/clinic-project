e5384d70f8d37f4ee8d0bfc25a25ec3e723d6325
KrisNathanielDimaapi <21-00385@g.batstate-u.edu.ph>
2023-11-19 18:17:08 +0800

    updated design of add transaction

--- pages/addTransaction.php
+++ pages/addTransaction.php
@@ -4,43 +4,50 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Transaction Record</title>
-    <link rel="stylesheet" href="../styles/transaction.css">
+    <link rel="stylesheet" href="../styles/requestUpdate.css">
     <?php include('header.php'); ?>
 </head>
 <body>
-    <div class="container">
-        <?php include('sidebar.php'); ?>
-        <main>
-            <h1>New Transaction</h1>
-            <div class="btn-cont">
-                <div class="button">
-                    <a href="transaction.php">Back</a>
-                </div>
+<div class="am-container">
+        <div class="am-body">
+            <div class="am-head">
+                <h1>Add Transaction</h1>
             </div>
-            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" class="form-container" id="forms">
-                <div>
-                    <span>Patient ID</span>
-                    <input type="number" id="patientid" name="patientid" placeholder="Enter Patient ID" required>
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST"  class="am-body-box" id="forms">
+            <a href="transaction.php"><i class="fas fa-arrow-alt-circle-left"></i></a>
+                <div class="am-row">
+                    <div class="am-col-6">
+                        <p>Patient ID: </p>
+                        <input type="number" id="patientid" name="patientid" placeholder="Enter Patient ID" required>
+                    </div>
+                    <div class="am-col-6">
+                        <p>Charge Amount: </p>
+                        <input type="number" id="chargeamount" name="chargeamount" placeholder="Enter Charge Amount" required>
+                    </div>
                 </div>
-                <div>
-                    <span>Charge Amount</span>
-                    <input type="number" id="chargeamount" name="chargeamount" placeholder="Enter Charge Amount" required>
+                <div class="am-row">
+                    <div class="am-col-6">
+                        <p>Amount Paid: </p>
+                        <input type="number" id="amountpaid" name="amountpaid" placeholder="Enter Amount Paid" required>
+                    </div>
+                    <div class="am-col-6">
+                        <p>Notes: </p>
+                        <textarea id="notes" name="notes" rows="3" cols="50" placeholder="Enter your Notes"></textarea>
+                    </div>
                 </div>
-                <div>
-                    <span>Amount Paid</span>
-                    <input type="number" id="amountpaid" name="amountpaid" placeholder="Enter Amount Paid" required>
-                </div>
-                <div>
-                    <span>Notes</span>
-                    <textarea id="notes" name="notes" rows="3" cols="50" placeholder="Enter your Notes"></textarea>
-                </div>
-                <div class="button btn-submit">
-                    <input type="submit" id="form-btn" name="submit" value="Submit">
+
+                <div class="am-row">
+                    <div class="am-col-3">
+                    <button type="submit" id="form-btn" name="submit" value="Submit">Add Transaction</button>
+                    </div>
                 </div>
             </form>
             <script>
                 insertAlert('form-btn', 'forms');
             </script>
+            <div class="am-footer">
+                    <p>Dra. Ruth Luneta-Alolod Dental Clinic</p>
+                </div>
         </main>
     </div>
 </body>
